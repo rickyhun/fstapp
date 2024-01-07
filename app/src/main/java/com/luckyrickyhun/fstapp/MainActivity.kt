128839b8a5749ced6cb72b9c580884a4157d1d2e
rickyhun <tomorrow_art@naver.com>
2024-01-08 00:50:37 +0900

    Initial commit

---
+++ app/src/main/java/com/luckyrickyhun/fstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luckyrickyhun.fstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
